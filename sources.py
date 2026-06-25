a1ee8d59634aaa6c3a7798e56254951c724f00fd
Fateh8787 <157641398+Fateh8787@users.noreply.github.com>
2026-06-26 02:19:13 +0330

    Update sources.py

--- sources.py
+++ sources.py
@@ -2,22 +2,41 @@ import feedparser
 
 FEEDS = [
     "https://www.talanews.com/feed",
+    "https://www.mehrnews.com/rss",
+    "https://www.tasnimnews.com/fa/rss/feed/0/7/0",
+    "https://www.irna.ir/rss",
+]
+
+KEYWORDS = [
+    "طلا",
+    "سکه",
+    "دلار",
+    "ارز",
+    "بانک مرکزی",
+    "طلای ۱۸ عیار",
+    "اونس",
 ]
 
 def get_news():
     news = []
 
-    for url in FEEDS:
+    for feed_url in FEEDS:
         try:
-            feed = feedparser.parse(url)
+            feed = feedparser.parse(feed_url)
 
             for item in feed.entries:
+                title = item.get("title", "")
+                link = item.get("link", "")
+
+                if not any(word in title for word in KEYWORDS):
+                    continue
+
                 news.append({
-                    "title": item.title,
-                    "link": item.link
+                    "title": title,
+                    "link": link
                 })
 
         except Exception as e:
-            print(e)
+            print("RSS ERROR:", e)
 
     return news
