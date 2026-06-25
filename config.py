e8b235ec74efd9eb74b2e6d74ac9aa8a98f20f8e
Fateh8787 <157641398+Fateh8787@users.noreply.github.com>
2026-06-26 01:56:17 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHANNEL_ID = "@goldiran87"
+
+CHECK_INTERVAL = 60  # هر ۶۰ ثانیه بررسی خبر
+
+SOURCES = [
+    "https://www.tgju.org/",
+    "https://www.tala.ir/",
+    "https://www.talanews.com/",
+]
